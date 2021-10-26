85055a6909ff1acd9f28c0709fcb8d8a1ac8d10b
eliskol <75465281+eliskol@users.noreply.github.com>
2021-10-26 15:49:00 +0000

    implemented graph tests

--- tests/graph_tests.py
+++ tests/graph_tests.py
@@ -1,6 +1,13 @@
 import sys
-sys.path[0] = '/home/runner/machine-learning/src'
+sys.path[0] = '/workspace/machine-learning/src'
 
 from graph import Graph
 
-graph_1 = Graph([[0, 1], [1, 2], [2, 3], [2, 4], [4, 0]])
+graph_1 = Graph([[0, 1], [1, 2], [2, 3], [2, 4], [4, 0], [5, 0]])
+graph_2 = Graph([(0, 1), (1, 0), (2, 3), (3, 0), (1, 4), (0, 4), (4, 5), (4, 6), (5, 6)])
+
+assert graph_1.get_children(2) == [3, 4]
+assert graph_1.get_parents(0) == [4, 5]
+
+assert graph_2.get_parents(0) == [1, 3]
+assert graph_2.get_children(4) == [5, 6]
