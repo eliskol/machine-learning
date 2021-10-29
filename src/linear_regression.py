fea7eebb1c66d5091bd761401b84270edb43694d
eliskol <75465281+eliskol@users.noreply.github.com>
2021-10-29 00:27:38 +0000

    made a roast beef and peanut butter sandwich

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -3,10 +3,16 @@ from matrix import Matrix
 class LinearRegressor:
 
     def fit(self, data):
-        y_matrix_rows = [[point[1]] for point in data]
+        y_matrix_rows = [[point[-1]] for point in data]
         y_matrix = Matrix(y_matrix_rows)
 
-        coefficient_matrix_rows = [[point[0], 1] for point in data]
+        coefficient_matrix_rows = []
+        for point in data:
+
+            row_to_append = point[:-1]
+            row_to_append.append(1)
+            coefficient_matrix_rows.append(row_to_append)
+
         coefficient_matrix = Matrix(coefficient_matrix_rows)
 
         transpose_times_y = coefficient_matrix.transpose().matrix_multiply(y_matrix)
@@ -14,11 +20,14 @@ class LinearRegressor:
 
         m_and_b_matrix = transpose_times_coefficients.inverse().matrix_multiply(transpose_times_y)
 
-        self.coefficients = []
-        self.coefficients.append(m_and_b_matrix.rows[1][0])
-        self.coefficients.append(m_and_b_matrix.rows[0][0])
+        self.coefficients = [coefficient[0] for coefficient in m_and_b_matrix.rows[::-1]]
 
-    def predict(self, x_value):
+
+    def predict(self, point_to_predict_at):
         if self.coefficients == None: return "no dtata to fit"
-
-        return self.coefficients[1] * x_value + self.coefficients[0]
+        if len(point_to_predict_at) != len(self.coefficients) - 1: return ":cursed:"
+        answer = 0
+        for i in range(0, len(point_to_predict_at)):
+            answer += point_to_predict_at[i] * self.coefficients[i + 1]
+        answer += self.coefficients[0]
+        return answer
