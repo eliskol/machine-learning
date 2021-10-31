990711cbe13eb5cf4cd208b6fa95a7b3dc9c3c1f
eliskol <75465281+eliskol@users.noreply.github.com>
2021-10-31 22:10:27 +0000

    bruh

--- tests/bfs_and_dfs_tests.py
+++ tests/bfs_and_dfs_tests.py
@@ -7,3 +7,4 @@ from graph import *
 graph_1 = Graph([[4, 0], [4, 8], [4, 6], [0, 8], [0, 2], [0, 3], [2, 3], [3, 2], [6, 3], [3, 1], [3, 9], [3, 5], [5, 7]])
 print(graph_1.breadth_first(4))
 print(graph_1.depth_first(4))
+print(graph_1.depth_first(8))
