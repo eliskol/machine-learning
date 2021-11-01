85e385d9c0f01f6235d9ebc5e054a38df6285028
eliskol <75465281+eliskol@users.noreply.github.com>
2021-11-01 22:21:52 +0000

    impl bfs and dfs tests

--- tests/bfs_and_dfs_tests.py
+++ tests/bfs_and_dfs_tests.py
@@ -5,6 +5,9 @@ from graph import *
 
 
 graph_1 = Graph([[4, 0], [4, 8], [4, 6], [0, 8], [0, 2], [0, 3], [2, 3], [3, 2], [6, 3], [3, 1], [3, 9], [3, 5], [5, 7]])
-print(graph_1.breadth_first(4))
-print(graph_1.depth_first(4))
-print(graph_1.depth_first(8))
+graph_2 = Graph([[0, 1], [0, 2], [0, 3], [1, 2], [2, 3], [3, 4], [3, 5], [3, 6], [6, 7], [6, 8]])
+
+assert graph_1.breadth_first(4) == [4, 0, 8, 6, 2, 3, 1, 9, 5, 7]
+assert graph_1.depth_first(4) == [4, 6, 3, 5, 7, 9, 1, 2, 8, 0]
+assert graph_2.breadth_first(0) == [0, 1, 2, 3, 4, 5, 6, 7, 8]
+assert graph_2.depth_first(0) == [0, 3, 6, 8, 7, 5, 4, 2, 1]
