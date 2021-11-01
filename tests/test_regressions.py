8a710e080fb05216758427ad7e64576bd10bf163
eliskol <75465281+eliskol@users.noreply.github.com>
2021-11-01 22:36:12 +0000

    3d linear regression tests

--- tests/test_regressions.py
+++ tests/test_regressions.py
@@ -1,34 +1,43 @@
 import sys
-sys.path[0] = '/workspace/machine-learning/src'
+sys.path[0] = '/home/coder/project/src'
 
 import math
 
 from linear_regression import LinearRegressor
 from logistic_regression import LogisticRegressor
 
-bruh = LinearRegressor()
-bruh.fit([[1, 0.2], [2, 0.25], [3, 0.5]])
+# bruh = LinearRegressor()
+# bruh.fit([[1, 0.2], [2, 0.25], [3, 0.5]])
 
-assert math.isclose(bruh.coefficients[0], 0.01666666666, rel_tol=0.01)
-assert math.isclose(bruh.coefficients[1], 0.15, rel_tol=0.00001)
-assert math.isclose(bruh.predict([4]), 0.616666, rel_tol=0.0001)
+# assert math.isclose(bruh.coefficients[0], 0.01666666666, rel_tol=0.01)
+# assert math.isclose(bruh.coefficients[1], 0.15, rel_tol=0.00001)
+# assert math.isclose(bruh.predict([4]), 0.616666, rel_tol=0.0001)
 
-bruh2 = LinearRegressor()
-bruh2.fit([[0.7, 0.9], [0.752, 0.69834], [1238, 1289]])
+# bruh2 = LinearRegressor()
+# bruh2.fit([[0.7, 0.9], [0.752, 0.69834], [1238, 1289]])
 
-assert math.isclose(bruh2.coefficients[0], 0.04329, rel_tol=0.0001)
-assert math.isclose(bruh2.coefficients[1], 1.0412, rel_tol=0.0001)
-assert math.isclose(bruh2.predict([9]), 9.41409, rel_tol=0.0001)
+# assert math.isclose(bruh2.coefficients[0], 0.04329, rel_tol=0.0001)
+# assert math.isclose(bruh2.coefficients[1], 1.0412, rel_tol=0.0001)
+# assert math.isclose(bruh2.predict([9]), 9.41409, rel_tol=0.0001)
 
-bruh3 = LogisticRegressor()
-bruh3.fit([[1, 0.2], [2, 0.25], [3, 0.5]])
+# bruh3 = LogisticRegressor()
+# bruh3.fit([[1, 0.2], [2, 0.25], [3, 0.5]])
 
-assert math.isclose(bruh3.coefficients[0], 2.21459, rel_tol=0.0001)
-assert math.isclose(bruh3.coefficients[1], -0.693147, rel_tol=0.0001)
-assert math.isclose(bruh3.predict([2]), 0.304004, rel_tol=0.0001)
+# assert math.isclose(bruh3.coefficients[0], 2.21459, rel_tol=0.0001)
+# assert math.isclose(bruh3.coefficients[1], -0.693147, rel_tol=0.0001)
+# assert math.isclose(bruh3.predict([2]), 0.304004, rel_tol=0.0001)
 
-bruh4 = LogisticRegressor()
-bruh4.fit([[5, 0.6], [3, 0.4], [1, 0.2], [7, 0.8]])
-assert math.isclose(bruh4.coefficients[0], 1.82573, rel_tol=0.0001)
-assert math.isclose(bruh4.coefficients[1], -0.45643, rel_tol=0.0001)
-assert math.isclose(bruh4.predict([7]), 0.79726, rel_tol=0.0001)
+# bruh4 = LogisticRegressor()
+# bruh4.fit([[5, 0.6], [3, 0.4], [1, 0.2], [7, 0.8]])
+# assert math.isclose(bruh4.coefficients[0], 1.82573, rel_tol=0.0001)
+# assert math.isclose(bruh4.coefficients[1], -0.45643, rel_tol=0.0001)
+# assert math.isclose(bruh4.predict([7]), 0.79726, rel_tol=0.0001)
+
+bruh5 = LinearRegressor()
+bruh5.fit([[0, 0, 1], [1, 0, 2], [2, 0, 4], [4, 0, 8], [0, 8, 6]])
+assert bruh5.coefficients == [0.5999999999999996, 0.675, 1.799999999999999]
+
+
+bruh6 = LinearRegressor()
+bruh6.fit([[0, 0, 1], [1, 0, 2], [2, 0, 4], [4, 0, 8], [0, 8, 6], [0, 6, 7]])
+assert bruh6.coefficients == [0.8203125, 0.78515625, 1.7265624999999991]
