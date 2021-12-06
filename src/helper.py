f14f9ce0cba1c12aff0d05946af994ce908f4ec3
Elias <75465281+eliskol@users.noreply.github.com>
2021-12-06 23:37:03 +0000

    find min in list helper func

--- src/helper.py
+++ src/helper.py
@@ -4,3 +4,11 @@ def put_last_entry_first(list):
     for i in range(0, len(list) - 1):
         output_list.append(list[i])
     return output_list
+
+
+def find_min(arr):
+    min = arr[0]
+    for i, entry in enumerate(arr):
+        if entry < min:
+            min = arr[i]
+    return min
