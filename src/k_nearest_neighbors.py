8586cd9a175c0f61710ff6d4ed0b120004dd2be5
Elias <75465281+eliskol@users.noreply.github.com>
2021-12-07 23:41:19 +0000

    implement knn

--- src/k_nearest_neighbors.py
+++ src/k_nearest_neighbors.py
@@ -1,7 +1,7 @@
 from helper import find_min
 
 
-class KNearestNeighborsClassifier(k):
+class KNearestNeighborsClassifier:
 
     def __init__(self, k):
         self.k = k
@@ -22,9 +22,18 @@ class KNearestNeighborsClassifier(k):
     def classify(self, input_point):
         distances = self.compute_distances(input_point)
         classifications = []
-        for i in range(k):
+        for i in range(self.k):
             smallest_distance = find_min(distances)
             index_of_smallest_distance = distances.index(smallest_distance)
             classifications.append(self.classifications[index_of_smallest_distance])
             distances.pop(index_of_smallest_distance)
-        return 
+
+        classification_count_dict = {classification: 0 for classification in classifications}
+        classification_with_max_count = classifications[0]
+
+        for classification in classifications:
+            classification_count_dict[classification] += 1
+            if classification_count_dict[classification] > classification_count_dict[classification_with_max_count]:
+                classification_with_max_count = classification
+
+        return classification_with_max_count
