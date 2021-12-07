83d81062b913b5136ee9997b1dbd5438d209824d
Elias <75465281+eliskol@users.noreply.github.com>
2021-12-07 23:41:50 +0000

    added matrix tests that use overloading

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -16,20 +16,19 @@ matrix_9 = Matrix([[5 , 4 , 2 , 4 , 6 , 3 , 8 , 5 , 2 , 7 , 9 , 3 , 0 , 9 , 8], 
 # assert matrix_1.transpose().rows == [[3, 4, 5], [2, 4, 6]]
 # assert matrix_2.transpose().rows == [[4, 0], [0, 6], [2, 3]]
 
-# assert matrix_1.add(matrix_2) == "invalid matrix dimensions"
-# assert matrix_3.add(matrix_4).rows == [[5, 5], [5, 5]]
+assert (matrix_1 + matrix_2) == "invalid matrix dimensions"
+assert (matrix_3 + matrix_4).rows == [[5, 5], [5, 5]]
 
-# assert matrix_1.subtract(matrix_3) == "invalid matrix dimensions"
-# assert matrix_1.subtract(matrix_5).rows == [[2, 0], [1, 0], [0, 0]]
+assert (matrix_1 - matrix_3) == "invalid matrix dimensions"
+assert (matrix_1 - matrix_5).rows == [[2, 0], [1, 0], [0, 0]]
 
-# assert matrix_5.scalar_multiply(2).rows == [[2, 4], [6, 8], [10, 12]]
-# assert matrix_2.scalar_multiply(6).rows == [[24, 0, 12], [0, 36, 18]]
+assert (matrix_5 * 2).rows == [[2, 4], [6, 8], [10, 12]]
+assert (matrix_2 * 6).rows == [[24, 0, 12], [0, 36, 18]]
 
-assert matrix_4.matrix_multiply(matrix_3).rows == [[13, 20], [5, 8]]
-bruh = matrix_4 @ matrix_3
-print(bruh.rows)
-# assert matrix_2.matrix_multiply(matrix_1).rows == [[22, 20], [39, 42]]
+assert (matrix_4 @ matrix_3).rows == [[13, 20], [5, 8]]
+assert (matrix_2 @ matrix_1).rows == [[22, 20], [39, 42]]
 
+assert Matrix([[1, 2], [3, 4]]) == Matrix([[1, 2], [3, 4]])
 
 # assert matrix_3.calc_determinant_recursive() == -2
 # assert matrix_2.calc_determinant_recursive() == "invalid matrix dimensions (calc_determinant_recursive)"
