af68b76d4387e4488d7bcabeed5ab9d6a76bb27d
Elias <75465281+eliskol@users.noreply.github.com>
2021-12-14 20:56:09 +0000

    higher order grad desc in prog

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -3,3 +3,15 @@ def minimize(fprime, x0, learning_rate=0.001, num_iterations=1000):
     for i in range(num_iterations):
         x_old = x_old - (learning_rate * fprime(x_old))
     return x_old
+
+
+def two_var_minimize(fx, fy, x0, y0, learning_rate=0.001, num_iterations=1000):
+
+
+
+def multivar_minimize(gradient, v0, learning_rate=0.001, num_iterations=1000):
+    v_old = v0
+    for i in range(num_iterations):
+        for j, coord in enumerate(v_old):
+            v_old[i] = coord - learning_rate * gradient(v_old)[j]
+    return v_old
