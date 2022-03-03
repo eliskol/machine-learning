5c19e9ee8bc7f5a8087f1507088cf5e71c2b7303
Elias <75465281+eliskol@users.noreply.github.com>
2022-03-03 22:31:35 +0000

    reformatted graph.py in accordance with PEP8

--- src/graph.py
+++ src/graph.py
@@ -1,6 +1,3 @@
-# import sys
-# sys.path.insert(1, "/workspace/machine-learning/src")
-
 from queue_a import Queue
 from stack import *
 
@@ -8,24 +5,23 @@ from stack import *
 class Graph:
     def __init__(self, edges):
 
-        self.children_by_id = {edge[0] : [] for edge in edges}
+        self.children_by_id = {edge[0]: [] for edge in edges}
 
-        self.parents_by_id = {edge[1] : [] for edge in edges}
-        
+        self.parents_by_id = {edge[1]: [] for edge in edges}
+
         for edge in edges:
             self.children_by_id[edge[0]].append(edge[1])
             self.parents_by_id[edge[1]].append(edge[0])
 
-
     def get_children(self, node):
-        if node in self.children_by_id: return self.children_by_id[node]
+        if node in self.children_by_id:
+            return self.children_by_id[node]
         return None
-
 
     def get_parents(self, node):
-        if node in self.parents_by_id: return self.parents_by_id[node]
+        if node in self.parents_by_id:
+            return self.parents_by_id[node]
         return None
-
 
     def breadth_first(self, node):
         queue = Queue([node])
@@ -40,15 +36,16 @@ class Graph:
 
             node_children = self.get_children(dequeued_node)
 
-            if node_children is None: continue
+            if node_children is None:
+                continue
 
             for child in node_children:
-                if child in visited: continue
+                if child in visited:
+                    continue
                 queue.enqueue(child)
                 visited[child] = True
 
         return order
-
 
     def depth_first(self, node):
         stack = Stack([node])
@@ -63,10 +60,12 @@ class Graph:
 
             top_element_children = self.get_children(top_element)
 
-            if top_element_children is None: continue
+            if top_element_children is None:
+                continue
 
             for child in top_element_children:
-                if child in visited: continue
+                if child in visited:
+                    continue
                 stack.push(child)
                 visited[child] = True
 
