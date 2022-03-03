06e2a5f7448500c3a1eab3c60fe353ae9d3df11b
Elias <75465281+eliskol@users.noreply.github.com>
2022-03-03 23:39:50 +0000

    calc_distance and calc_shortest_path in progress

--- src/graph.py
+++ src/graph.py
@@ -1,5 +1,6 @@
 from queue_a import Queue
 from stack import *
+from node import Node
 
 
 class Graph:
@@ -9,7 +10,13 @@ class Graph:
 
         self.parents_by_id = {edge[1]: [] for edge in edges}
 
+        self.nodes_by_id = {}
+
         for edge in edges:
+            if edge[0] not in self.nodes_by_id:
+                self.nodes_by_id[edge[0]] = Node(edge[0])
+            if edge[1] not in self.nodes_by_id:
+                self.nodes_by_id[edge[1]] = Node(edge[1])
             self.children_by_id[edge[0]].append(edge[1])
             self.parents_by_id[edge[1]].append(edge[0])
 
@@ -67,6 +74,36 @@ class Graph:
                 if child in visited:
                     continue
                 stack.push(child)
+                visited[child] = True
+
+        return order
+
+    def set_distance_and_previous(self, start_index):
+
+        queue = Queue([start_index])
+        visited = {}
+        order = []
+
+        self.nodes_by_id[start_index].distance += 1
+
+        while queue.contents != []:
+
+            dequeued_node = queue.dequeue()
+            order.append(dequeued_node)
+            visited[dequeued_node] = True
+            node_children = self.get_children(dequeued_node)
+            current_node = Node(dequeued_node)
+
+            if node_children is None:
+                continue
+
+            for child in node_children:
+                if child in visited:
+                    continue
+                self.nodes_by_id[child].previous = current_node
+                self.nodes_by_id[child].distance += 1
+
+                queue.enqueue(child)
                 visited[child] = True
 
         return order
