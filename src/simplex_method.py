a2d4bc75e33fb86b56010ef97daa09c3c9864c6c
Elias <75465281+eliskol@users.noreply.github.com>
2022-09-28 22:39:20 +0000

    made sure that strictest constraint is not negative (fixed simplex)

--- src/simplex_method.py
+++ src/simplex_method.py
@@ -1,9 +1,10 @@
+from operator import indexOf
 from matrix import Matrix
 
 
 def simplex_method(initial_array):
     simplex_matrix = Matrix(initial_array)
-    while max(simplex_matrix.rows[0]) > 0:
+    while max(simplex_matrix.rows[0][0:-1]) > 1e-8:
         index_of_variable_with_largest_slope = simplex_matrix.rows[0].index(max(simplex_matrix.rows[0]))
         index_of_strictest_constraint_row = 1
 
@@ -13,7 +14,7 @@ def simplex_method(initial_array):
 
             strictest_constraint_so_far = simplex_matrix.rows[index_of_strictest_constraint_row][-1] / simplex_matrix.rows[index_of_strictest_constraint_row][index_of_variable_with_largest_slope]
 
-            if current_row_constraint < strictest_constraint_so_far:
+            if current_row_constraint < strictest_constraint_so_far and current_row_constraint > 0:
                 index_of_strictest_constraint_row = i
 
         simplex_matrix.clear_above(index_of_strictest_constraint_row, index_of_variable_with_largest_slope)
@@ -27,4 +28,12 @@ simplex_test_array = [[1, 2, 1, 0, 0, 0, 0],
                       [4, 2, 3, 0, 1, 0, 28],
                       [2, 5, 5, 0, 0, 1, 30]]
 
-print(simplex_method(simplex_test_array))
+# print(simplex_method(simplex_test_array))
+
+simplex_test_array2 = [[20, 10, 15, 0, 0, 0, 0, 0],
+                       [3, 2, 5, 1, 0, 0, 0, 55],
+                       [2, 1, 1, 0, 1, 0, 0, 26],
+                       [1, 1, 3, 0, 0, 1, 0, 30],
+                       [5, 2, 4, 0, 0, 0, 1, 57]]
+
+simplex_method(simplex_test_array2)
