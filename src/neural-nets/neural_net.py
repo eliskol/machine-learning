30b666eeae2c067c09d690e4618c99f33aef6ebf
Elias <75465281+eliskol@users.noreply.github.com>
2023-04-24 22:43:06 +0000

    trying to get gradient descent working

--- src/neural-nets/neural_net.py
+++ src/neural-nets/neural_net.py
@@ -1,6 +1,55 @@
 import math
 import numpy as np
 
+sigmoid = np.vectorize(lambda x: 1 / (1 + math.e ** -x))
+sigmoid_prime = np.vectorize(lambda x: (
+    math.e ** -x) / (1 + math.e ** -x) ** 2)
+
+
+class NeuralNet:
+    def __init__(self, A, b, datapoints, learning_rate):
+        self.A = A
+        self.b = b
+        self.datapoints = datapoints
+        self.learning_rate = learning_rate
+        self.dRSS_da = [np.zeros(matrix.shape) for matrix in self.A]
+        self.dRSS_db = [np.zeros(matrix.shape) for matrix in self.b]
+        for [x, y] in datapoints:
+            self.Sigma, self.h = self.propagate_forward(self.A, self.b, x, y)
+            self.dRSS_dh = self.propagate_backward(self.A, self.b, x, y, self.Sigma, self.h)
+            dRSS_da, dRSS_db = self.expand_neuron_gradients_to_weight_gradients(self.dRSS_dh, self.Sigma, self.h)
+            for l in range(len(self.dRSS_da)):
+                self.dRSS_da[l] += dRSS_da[l]
+                self.dRSS_db[l] += dRSS_db[l]
+
+    def propagate_forward(self, A, b, x, y):
+        Sigma = [None]
+        h = [x]
+        for i in range(len(A)):
+            Sigma.append(A[i] * h[i] + b[i])
+            h.append(sigmoid(Sigma[i + 1]))
+        return Sigma, h
+
+    def propagate_backward(self, A: list[np.matrix], b, x, y, Sigma: list[np.matrix], h: list[np.matrix]):
+        dRSS_dh = []
+        dRSS_dh.append(2 * (h[-1] - y))
+        for l, A_l in enumerate(reversed(A[1:])):
+            dRSS_dh.append(A_l.transpose() *
+                        (np.multiply(dRSS_dh[-1], sigmoid_prime(Sigma[-l - 1]))))
+        return dRSS_dh[::-1]
+
+    def expand_neuron_gradients_to_weight_gradients(self, dRSS_dh, Sigma, h):
+        dRSS_db = []
+        for l in range(0, len(h) - 1):
+            dRSS_db.append(np.multiply(dRSS_dh[l], sigmoid_prime(Sigma[l + 1])))
+        dRSS_da = []
+        for l in range(0, len(h) - 1):
+            dRSS_da.append(np.matrix(np.outer(dRSS_db[l], h[l])))
+        return dRSS_db, dRSS_da
+
+    # def descend_gradient_to_new_weights:
+    #     for l, weight_matrix in enumerate(self.A):
+    #         weight_matrix -= self.dRSS
 
 A_1 = np.matrix([[5], [-5], [5], [-5]])
 A_2 = np.matrix([[10, 10, 0, 0], [0, 0, 10, 10]])
@@ -14,42 +63,9 @@ b_3 = np.matrix([-2.5])
 
 b = [b_1, b_2, b_3]
 
-x = np.matrix(0)
-y = 0
+datapoints = [[np.matrix(0), 0], [np.matrix(0.25), 1], [np.matrix(0.5), 0.5], [np.matrix(0.75), 1], [np.matrix(1), 0]]
 
-sigmoid = np.vectorize(lambda x: 1 / (1 + math.e ** -x))
-sigmoid_prime = np.vectorize(lambda x: (
-    math.e ** -x) / (1 + math.e ** -x) ** 2)
+learning_rate = 0.01
 
-
-def propagate_forward(A, b, x, y):
-    Sigma = [None]
-    h = [x]
-    for i in range(len(A)):
-        Sigma.append(A[i] * h[i] + b[i])
-        h.append(sigmoid(Sigma[i + 1]))
-    return Sigma, h
-
-
-def propagate_backward(A: list[np.matrix], b, x, y, Sigma: list[np.matrix], h: list[np.matrix]):
-    dRSS_dh = []
-    dRSS_dh.append(2 * (h[-1] - y))
-    for l, A_l in enumerate(reversed(A[1:])):
-        dRSS_dh.append(A_l.transpose() *
-                       (np.multiply(dRSS_dh[-1], sigmoid_prime(Sigma[-l - 1]))))
-    return dRSS_dh[::-1]
-
-
-def expand_neuron_gradients_to_weight_gradients(dRSS_dh, Sigma, h):
-    dRSS_db = []
-    for l in range(0, len(h) - 1):
-        dRSS_db.append(np.multiply(dRSS_dh[l], sigmoid_prime(Sigma[l + 1])))
-    dRSS_da = []
-    for l in range(0, len(h) - 1):
-        dRSS_da.append(np.matrix(np.outer(dRSS_db[l], h[l])))
-    return dRSS_db, dRSS_da
-
-
-Sigma, h = propagate_forward(A, b, x, y)
-dRSS_dh = propagate_backward(A, b, x, y, Sigma, h)
-print(expand_neuron_gradients_to_weight_gradients(dRSS_dh, Sigma, h)[1])
+bruh = NeuralNet(A, b, datapoints, learning_rate)
+print(bruh.dRSS_da)
