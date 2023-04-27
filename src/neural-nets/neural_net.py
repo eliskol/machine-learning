d654b2686dda551c6ca00d766e6431cd6b4137e3
Elias <75465281+eliskol@users.noreply.github.com>
2023-04-27 00:20:50 +0000

    made activation functions configurable by layer; combined unnecessary loops; added compute_RSS method

--- src/neural-nets/neural_net.py
+++ src/neural-nets/neural_net.py
@@ -2,40 +2,41 @@ import math
 import numpy as np
 import time
 
-sigmoid = np.vectorize(lambda x: 1 / (1 + math.e ** -x))
-sigmoid_prime = np.vectorize(lambda x: (
-    math.e ** -x) / (1 + math.e ** -x) ** 2)
-
 
 class NeuralNet:
-    def __init__(self, A, b, datapoints, learning_rate):
+    def __init__(self, A, b, activation_functions_and_derivatives: list[np.vectorize], datapoints, learning_rate):
         self.A = A
         self.b = b
+        self.activation_functions_and_derivatives = activation_functions_and_derivatives
+        # should be [[activation func for 1st layer, derivative], [2nd layer, derivative], .. etc]; currently can't do diff activation functions for diff neurons on same layer
         self.datapoints = datapoints
         self.learning_rate = learning_rate
+        self.number_of_weights = sum([matrix.shape[0] * matrix.shape[1] for matrix in self.A]) + sum([matrix.shape[0] * matrix.shape[1] for matrix in self.b])
 
     def propagate_forward(self, A, b, x, y):
         Sigma = [None]
         h = [x]
         for i in range(len(A)):
+            activation_function = self.activation_functions_and_derivatives[i][0]
             Sigma.append(A[i] * h[i] + b[i])
-            h.append(sigmoid(Sigma[i + 1]))
+            h.append(activation_function(Sigma[i + 1]))
         return Sigma, h
 
     def propagate_backward(self, A: list[np.matrix], b, x, y, Sigma: list[np.matrix], h: list[np.matrix]):
         dRSS_dh = []
         dRSS_dh.append(2 * (h[-1] - y))
         for l, A_l in enumerate(reversed(A[1:])):
+            activation_function_derivative = self.activation_functions_and_derivatives[l][1]
             dRSS_dh.append(A_l.transpose() *
-                        (np.multiply(dRSS_dh[-1], sigmoid_prime(Sigma[-l - 1]))))
+                        (np.multiply(dRSS_dh[-1], activation_function_derivative(Sigma[-l - 1]))))
         return dRSS_dh[::-1]
 
     def expand_neuron_gradients_to_weight_gradients(self, dRSS_dh, Sigma, h):
         dRSS_db = []
-        for l in range(0, len(h) - 1):
-            dRSS_db.append(np.multiply(dRSS_dh[l], sigmoid_prime(Sigma[l + 1])))
         dRSS_da = []
         for l in range(0, len(h) - 1):
+            activation_function_derivative = self.activation_functions_and_derivatives[l][1]
+            dRSS_db.append(np.multiply(dRSS_dh[l], activation_function_derivative(Sigma[l + 1])))
             dRSS_da.append(np.matrix(np.outer(dRSS_db[l], h[l])))
         return dRSS_db, dRSS_da
 
@@ -61,14 +62,13 @@ class NeuralNet:
         for i in range(iterations):
             self.sum_weight_gradients()
             self.descend_gradient_to_new_weights()
-            if time.time() - data[0] > 1:
-                print(i, 'completed;', (i - data[1]) / (time.time() - data[0]), 'per second in roughly last second;', (i + 1) / (time.time() - beginning), 'iter/s overall')
-                data = [time.time(), i]
-            # print('predictions:')
-            # for point in self.datapoints:
-            #     print(np.matrix.round(self.propagate_forward(self.A, self.b, point[0], point[1])[1][-1], decimals=3))
-            # print()
+            # if time.time() - data[0] > 3:
+            #     print(i, 'completed;', (i - data[1]) / (time.time() - data[0]), 'per second in last interval;', (i + 1) / (time.time() - beginning), 'iter/s overall')
+            #     data = [time.time(), i]
         print('training completed!')
 
     def predict(self, x):
         return self.propagate_forward(self.A, self.b, x, None)[1][-1]
+
+    def compute_RSS(self):
+        return sum([(self.predict(x) - y) ** 2 for x, y in self.datapoints])
